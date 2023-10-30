2da233e78c25c045691fa9be440db26413839a79
Aleksandr Anisimov <anisimov.alexander.s@gmail.com>
2023-10-30 09:13:50 +0300

    Fix reading function after debugging

--- hx710b.c
+++ hx710b.c
@@ -5,6 +5,7 @@
 
 void hx710b_power_on(hx710b_dev_t *dev)
 {
+    dev->rate = HX710B_RATE_10Hz;
     dev->clk_pin_reset();
     dev->delay_us(100);
 }
@@ -41,13 +42,13 @@ static void pulse(hx710b_dev_t *dev)
 uint32_t hx710b_read_raw_data(hx710b_dev_t *dev)
 {
     uint32_t hx710b_data = 0;
-    int pin_val = 0;
+    uint32_t pin_val = 0;
 
-    for (int i = 0; i < 24; ++i) {
+    for (int i = 0; i < HX710B_ADC_BITS; ++i) {
         dev->clk_pin_set();
         dev->delay_us(HX710B_CLOCK_PULSE_US);
-        dev->dout_pin_read();
-        hx710b_data |= pin_val << (24 - i - 1);
+        pin_val = dev->dout_pin_read();
+        hx710b_data |= pin_val << (HX710B_ADC_BITS - i - 1);
         dev->clk_pin_reset();
         dev->delay_us(HX710B_CLOCK_PULSE_US);
     }
@@ -55,6 +56,7 @@ uint32_t hx710b_read_raw_data(hx710b_dev_t *dev)
     if (dev->rate == HX710B_RATE_10Hz) {
         pulse(dev);
     } else {
+        pulse(dev);
         pulse(dev);
         pulse(dev);
     }
