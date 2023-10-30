16300e99ad390e99fe9e537961591e375c6ff9ed
Aleksandr Anisimov <anisimov.alexander.s@gmail.com>
2023-10-30 08:52:47 +0300

    Implement hx710b_read_raw_data()

--- hx710b.c
+++ hx710b.c
@@ -1,6 +1,7 @@
 #include "hx710b.h"
 
 #define HX710B_ADC_BITS (24U)
+#define HX710B_CLOCK_PULSE_US (20U)
 
 void hx710b_power_on(hx710b_dev_t *dev)
 {
@@ -29,8 +30,34 @@ int hx710b_is_new_data_available(hx710b_dev_t *dev)
     return 0;
 }
 
+static void pulse(hx710b_dev_t *dev)
+{
+    dev->clk_pin_set();
+    dev->delay_us(HX710B_CLOCK_PULSE_US);
+    dev->clk_pin_reset();
+    dev->delay_us(HX710B_CLOCK_PULSE_US);
+}
+
 uint32_t hx710b_read_raw_data(hx710b_dev_t *dev)
 {
-    uint32_t adc_data = 0;
-    return adc_data;
+    uint32_t hx710b_data = 0;
+    int pin_val = 0;
+
+    for (int i = 0; i < 24; ++i) {
+        dev->clk_pin_set();
+        dev->delay_us(HX710B_CLOCK_PULSE_US);
+        dev->dout_pin_read();
+        hx710b_data |= pin_val << (24 - i - 1);
+        dev->clk_pin_reset();
+        dev->delay_us(HX710B_CLOCK_PULSE_US);
+    }
+
+    if (dev->rate == HX710B_RATE_10Hz) {
+        pulse(dev);
+    } else {
+        pulse(dev);
+        pulse(dev);
+    }
+
+    return hx710b_data;
 }
